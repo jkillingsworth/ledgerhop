c3772fe87995d6af1e4e0a347d4cf600727c72ad
Jim Killingsworth <jkillingsworth@gmail.com>
2017-07-23 13:23:45 -0400

    Add ripple account lines command

--- ledgerhop.go
+++ ledgerhop.go
@@ -90,14 +90,66 @@ func handleRipplePing(w http.ResponseWriter, r *http.Request) {
 	executeTemplate(w, "ripple/ping.html", data)
 }
 
+func handleRippleAccountLines(w http.ResponseWriter, r *http.Request) {
+
+	var req string
+	var res string
+
+	format := func(b []byte) string {
+		var buffer bytes.Buffer
+		e := json.Indent(&buffer, b, "", "  ")
+		if e != nil {
+			log.Fatal(e)
+		}
+		return string(buffer.Bytes())
+	}
+
+	if r.Method == "POST" {
+
+		r.ParseForm()
+
+		command := struct {
+			ID      string `json:"id"`
+			Command string `json:"command"`
+			Ledger  string `json:"ledger_index"`
+			Account string `json:"account"`
+		}{
+			ID:      r.Form.Get("id"),
+			Command: r.Form.Get("command"),
+			Ledger:  r.Form.Get("ledger"),
+			Account: r.Form.Get("account"),
+		}
+
+		bytes, e := json.Marshal(command)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		req = format(bytes)
+		res = format(ripple.Execute(bytes))
+	}
+
+	data := struct {
+		Req string
+		Res string
+	}{
+		req,
+		res,
+	}
+
+	executeTemplate(w, "ripple/account-lines.html", data)
+}
+
 func init() {
 	registerTemplate(templates, "index.html")
 	registerTemplate(templates, "ripple/ping.html")
+	registerTemplate(templates, "ripple/account-lines.html")
 }
 
 func main() {
 	http.HandleFunc("/styles.css", handleStyles)
 	http.HandleFunc("/", handleIndex)
 	http.HandleFunc("/ripple/ping", handleRipplePing)
+	http.HandleFunc("/ripple/account-lines", handleRippleAccountLines)
 	http.ListenAndServe(":8080", nil)
 }
